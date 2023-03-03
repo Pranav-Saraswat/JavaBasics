22605d544ade9fd649d4ba0f0d7b34b50a80a570
Pranav Saraswat <pranavsaraswat20040124@gmail.com>
2023-03-03 15:55:39 +0530

    idk

--- Main.java
+++ Main.java
@@ -2,8 +2,36 @@
  * Print Hello World in Java
  */
 
-public class Main {
-    public static void main(String[] args) {
-      System.out.println("Hello World");
-    }
+ public class Main {
+  public static void main(String[] args) {
+    System.out.println("Hello World!");
   }
+}
+
+
+
+/**
+ * Print mutli lines
+ */
+
+
+ public class Main {
+  public static void main(String[] args) {
+    System.out.println("Hello World!");
+    System.out.println("I am learning Java.");
+    System.out.println("It is awesome!");
+  }
+}
+
+
+/**
+ * Print numbers
+ */
+
+ public class Main {
+  public static void main(String[] args) {
+    System.out.println(3);
+    System.out.println(358);
+    System.out.println(50000);
+  }
+}
