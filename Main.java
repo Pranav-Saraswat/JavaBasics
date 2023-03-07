d95096efa5ae9dc86ae4b8e614c9fd62a5a13549
Pranav Saraswat <pranavsaraswat20040124@gmail.com>
2023-03-07 19:21:51 +0530

    add some files

--- Main.java
+++ Main.java
@@ -2,11 +2,11 @@
  * Print Hello World in Java
  */
 
- public class Main {
-  public static void main(String[] args) {
-    System.out.println("Hello World!");
-  }
-}
+//  public class Main {
+//   public static void main(String[] args) {
+//     System.out.println("Hello World!");
+//   }
+// }
 
 
 
@@ -15,13 +15,13 @@
  */
 
 
- public class Main {
-  public static void main(String[] args) {
-    System.out.println("Hello World!");
-    System.out.println("I am learning Java.");
-    System.out.println("It is awesome!");
-  }
-}
+//  public class Main {
+//   public static void main(String[] args) {
+//     System.out.println("Hello World!");
+//     System.out.println("I am learning Java.");
+//     System.out.println("It is awesome!");
+//   }
+// }
 
 
 /**
